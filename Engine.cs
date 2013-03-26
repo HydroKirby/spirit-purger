3e84e983d23a119c9b35c1347fa1fe6bb4f0bfe0
L R <LeftRightAB@gmail.com>
2013-03-26 04:12:17 -0800

    Can go from the top menu to the main game.

--- Engine.cs
+++ Engine.cs
@@ -9,6 +9,17 @@ using SFML.Graphics;
 
 namespace TestSFMLDotNet
 {
+    /// <summary>
+    /// IComparer class for sorting integers backwards.
+    /// </summary>
+    public class ReversedSortInt : IComparer
+    {
+        public int Compare(object o1, object o2)
+        {
+            return (int)o2 - (int)o1;
+        }
+    }
+
     class Engine
     {
         // The ticks to wait after the redraw before updating the game.
@@ -34,7 +45,7 @@ namespace TestSFMLDotNet
         public const int BOMB_COMBO_DISPLAY_FRAMES = 270;
 
         // High-level stuff.
-        protected delegate void PaintHandler(object sender);
+        protected delegate void PaintHandler(object sender, double ticks);
         protected PaintHandler paintHandler = null;
         public KeyHandler keys = new KeyHandler();
         public Random rand = new Random();
@@ -133,6 +144,11 @@ namespace TestSFMLDotNet
 
         public void Run(RenderWindow app)
         {
+            // Apply extra things to the window.
+            app.SetKeyRepeatEnabled(false);
+            app.KeyPressed += new EventHandler<KeyEventArgs>(app_KeyPressed);
+            app.KeyReleased += new EventHandler<KeyEventArgs>(app_KeyReleased);
+
             // Load all resources.
             menuFont = new Font("arial.ttf");
             bg = LoadImage("bg.png");
@@ -208,9 +224,7 @@ namespace TestSFMLDotNet
              */
             Timer timer = new Timer();
             double lastUpdate = timer.GetTicks();
-            app.SetKeyRepeatEnabled(false);
-            app.KeyPressed += new EventHandler<KeyEventArgs>(app_KeyPressed);
-            app.KeyReleased += new EventHandler<KeyEventArgs>(app_KeyReleased);
+            
             Text text = new Text("test", menuFont, 12);
             text.Color = Color.Black;
             Color clearColor = new Color(250, 250, 250);
@@ -220,22 +234,10 @@ namespace TestSFMLDotNet
             app.DispatchEvents();
             while (app.IsOpen())
             {
-                // If it is time for a game update, update all gameplay.
-                if ((lastUpdate = timer.GetTicks()) >= UPDATE_TICKS)
-                {
-                    // DispatchEvents made all key states up-to-date.
-                    // Now, increment the time they were held down.
-                    keys.Update();
-//                    temp1 = Math.Max(temp1, keys.left);
-                    text.DisplayedString = keys.left.ToString();
-//                    text.DisplayedString = temp1.ToString();
-                    timer.Reset();
-                }
-
                 // Begin rendering.
                 // Clear screen
                 app.Clear(clearColor);
-                paintHandler(app);
+                paintHandler(app, lastUpdate);
 
                 // Draw the sprite
                 //app.Draw(sprite);
@@ -245,6 +247,23 @@ namespace TestSFMLDotNet
 
                 // Update the window
                 app.Display();
+
+                // Update the game.
+                // If it is time for a game update, update all gameplay.
+                if ((lastUpdate = timer.GetTicks()) >= UPDATE_TICKS)
+                {
+                    // DispatchEvents made all key states up-to-date.
+                    // Now, increment the time they were held down.
+                    keys.Update();
+                    if (gameState == GameState.GamePlay)
+                        UpdateGame(app, lastUpdate);
+                    else if (gameState == GameState.MainMenu)
+                        UpdateMenu(app, lastUpdate);
+//                    temp1 = Math.Max(temp1, keys.left);
+                    text.DisplayedString = keys.left.ToString();
+//                    text.DisplayedString = temp1.ToString();
+                    timer.Reset();
+                }
 
                 // Process events.
                 // If we are done, exit the loop immediately.
@@ -270,7 +289,181 @@ namespace TestSFMLDotNet
             keys.KeyUp(e.Code);
         }
 
-        protected void PaintMenu(object sender)
+        
+        /* --- Menu Logic --- */
+
+
+        /// <summary>
+        /// Updates the top menu.
+        /// </summary>
+        /// <param name="sender">The caller of this method.</param>
+        /// <param name="ticks">The ticks since the last call to this.</param>
+        protected void UpdateMenu(object sender, double ticks)
+        {
+            // The comparisons to 2 accounts for how KEY_UP and KEY_DOWN change
+            // the key hold-times to 1, but an immediately following Update()
+            // makes those 1s into 2s.
+
+            // The logic looks backwards for up/down, but it works.
+            if (keys.up == 2)
+            {
+                menuChoice = menuChoice <= 0 ? (int)MainMenu.EndChoices - 1 :
+                    menuChoice - 1;
+                disallowRapidSelection = false;
+            } if (keys.down == 2)
+            {
+                menuChoice = menuChoice > (int)MainMenu.EndChoices - 2 ? 0 :
+                    menuChoice + 1;
+                disallowRapidSelection = false;
+            }
+            // Disallow rapid selection while patch flag is on.
+            // But allow non-rapid selection even if patch flag is on.
+            // Rapid selection flag patch is disabled when any action is done
+            //   within the menu.
+            if (keys.shoot == 2)
+            {
+                if (disallowRapidSelection && keys.TappedShoot)
+                    return;
+                switch ((MainMenu)menuChoice)
+                {
+                    case MainMenu.Play:
+                        gameState = GameState.GamePlay;
+                        // Switch the delegate to painting the game.
+                        paintHandler = new PaintHandler(PaintGame);
+                        break;
+                    case MainMenu.GodMode:
+                        godMode = !godMode;
+                        break;
+                    case MainMenu.FunBomb:
+                        funBomb = !funBomb;
+                        break;
+                    case MainMenu.Repulsive:
+                        repulsive = !repulsive;
+                        break;
+                    case MainMenu.Exit:
+                        ((RenderWindow)sender).Close();
+                        break;
+                }
+                disallowRapidSelection = false;
+            }
+        }
+
+
+        /* --- Game Logic Code --- */
+        
+        
+        /// <summary>
+        /// Updates the game.
+        /// </summary>
+        /// <param name="sender">The caller of this method.</param>
+        /// <param name="ticks">The ticks since the last call to this.</param>
+        public void UpdateGame(object sender, double ticks)
+        {
+            if (paused)
+            {
+                if (keys.TappedBomb)
+                {
+                    // End the game.
+                    Reset();
+                    gameState = GameState.MainMenu;
+                    paintHandler = new PaintHandler(PaintMenu);
+                }
+                return;
+            }
+
+            /*
+            if (patternTime > 0 && transitionFrames <= 0)
+            {
+                prevSecondUpdateFraction += ticks;
+                if (prevSecondUpdateFraction >= 1.0)
+                {
+                    patternTime--;
+                    prevSecondUpdateFraction -= 1.0;
+                    if (patternTime == 0)
+                    {
+                        beatThisPattern = false;
+                        if (!boss.NextPattern())
+                        {
+                            bossState = BossState.Killed;
+                            gameOver = true;
+                        }
+                        else
+                            patternTime =
+                                Enemy.patternDuration[boss.currentPattern];
+                    }
+                }
+            }
+
+            MovePlayer();
+
+            if (godMode && keys.slow == 2 && bombBlast != null)
+                bombBlast = null;
+            if (gameOver)
+            {
+                if (keys.shoot == 2)
+                {
+                    this.Reset();
+                    gameState = GameState.MainMenu;
+                    paintHandler = new PaintHandler(PaintMenu);
+                    return;
+                }
+            }
+            else
+            {
+                if (keys.shoot > 0 && player.deathCountdown <= 0 &&
+                    player.reentryCountdown <= 0)
+                {
+                    if (player.TryShoot())
+                    {
+                        // The color (index) doesn't matter.
+                        Bullet bullet = new Bullet(0, 1, new Vector2D(
+                            player.location.X - Bullet.RADII[1],
+                            player.DrawLocation.Y),
+                            new Vector2D(Vector2D.DegreesToRadians(270)),
+                            9.0);
+                        playerBullets.Add(bullet);
+                        bullet = new Bullet(bullet);
+                        bullet.location.X = player.location.X +
+                            Bullet.RADII[1];
+                        playerBullets.Add(bullet);
+                    }
+                }
+                if (keys.bomb == 2 && bombBlast == null && (godMode ||
+                    bombs > 0 && player.reentryCountdown <= 0 &&
+                    player.deathCountdown <= 0))
+                {
+                    player.invincibleCountdown = Bomb.ACTIVE_FRAMES;
+                    bombBlast = new Bomb(player.location);
+                    bombCombo = 0;
+                    bombComboTimeCountdown = BOMB_COMBO_DISPLAY_FRAMES;
+                    bombs--;
+                    beatThisPattern = false;
+                    String leftoverBombs = "";
+                    for (int i = 0; i < bombs; i++)
+                        leftoverBombs += "☆";
+                    this.labelBombs.Text = leftoverBombs;
+                }
+            }
+
+            if (bombComboTimeCountdown >= 0)
+                bombComboTimeCountdown--;
+
+            UpdateBullets();
+            UpdateEnemies();
+            player.Update();
+             */
+        }
+
+
+        /* --- Rendering Code --- */
+
+
+        /// <summary>
+        /// Renders the top menu screen.
+        /// </summary>
+        /// <param name="sender">The caller of this method.</param>
+        /// <param name="ticks">The ticks since the last call to this.</param>
+        protected void PaintMenu(object sender, double ticks)
         {
             // Note: The app window is 290x290.
             int y = 130 + 15 * menuChoice;
@@ -300,6 +493,119 @@ namespace TestSFMLDotNet
             app.Draw(funBombText);
             app.Draw(repulsiveText);
             app.Draw(exitText);
+        }
+
+        /// <summary>
+        /// Renders the main game.
+        /// </summary>
+        /// <param name="sender">The caller of this method.</param>
+        /// <param name="ticks">The ticks since the last call to this.</param>
+        protected void PaintGame(object sender, double ticks)
+        {
+            /*
+            e.Graphics.DrawImage(bg, 0, 0);
+            // Draw the player, boss, and enemies.
+            if (lives >= 0)
+                player.Draw(e.Graphics);
+            if (bossState == BossState.Active)
+                boss.Draw(e.Graphics);
+            else if (bossState == BossState.Intro &&
+                     bossIntroTime > BOSS_PRE_INTRO_FRAMES)
+                // The boss is invisible unless it has waited more than
+                // BOSS_PRE_INTRO_FRAMES at which it then fades-in gradually.
+                // The visibility is the proprtion of time waited compared
+                // to BOSS_INTRO_FRAMES.
+                boss.Draw(e.Graphics,
+                    (int)((bossIntroTime - BOSS_PRE_INTRO_FRAMES) /
+                    (float)BOSS_INTRO_FRAMES * 255.0F));
+
+            // Draw the bullets and hitsparks.
+            foreach (Bullet spark in hitSparks)
+                if (spark.colorIndex == GRAZE_SPARK_INDEX)
+                    e.Graphics.DrawImage(grazeSparkImage, spark.DrawLocation);
+                else
+                    e.Graphics.DrawImage(bullseyeSparkImage,
+                                         spark.DrawLocation);
+            foreach (Bullet bullet in playerBullets)
+                e.Graphics.DrawImage(playerBulletImage, bullet.DrawLocation);
+            foreach (Bullet bullet in enemyBullets)
+                e.Graphics.DrawImage(
+                    bulletImages[bullet.SizeIndex][bullet.colorIndex],
+                    bullet.DrawLocation);
+
+            if (bombBlast != null)
+                bombBlast.Draw(e.Graphics);
+
+            if (keys.slow > 0)
+                e.Graphics.DrawImage(hitCircleImage,
+                    (int)((player.location.X - hitCircleImage.Width * 0.5)),
+                    (int)((player.location.Y - hitCircleImage.Height * 0.5)));
+
+            // Draw the HUD.
+            SolidBrush solidBrush = new SolidBrush(Color.Black);
+            // TODO: Fade-out when close-by.
+            e.Graphics.DrawString("Bullets:" +
+                (playerBullets.Count + enemyBullets.Count).ToString(),
+                Font, solidBrush, 0, this.ClientRectangle.Bottom - 14);
+            e.Graphics.DrawString("Grazed: " + grazeCount.ToString(), Font,
+                solidBrush, 0, this.ClientRectangle.Bottom - 25);
+            solidBrush.Dispose();
+
+            // Draw the boss' health bar.
+            if (!gameOver)
+            {
+                solidBrush = new SolidBrush(Color.Red);
+                e.Graphics.FillRectangle(solidBrush, 10, 28,
+                    (float)boss.health /
+                    Enemy.fullHealth[boss.currentPattern] *
+                    (this.ClientRectangle.Width - 20), 13);
+                solidBrush.Dispose();
+            }
+
+            // Draw the boss pattern time.
+            if (patternTime > 0)
+            {
+                solidBrush = new SolidBrush(Color.Red);
+                e.Graphics.DrawString(patternTime.ToString(), Font,
+                    solidBrush, 265.0F, 1.0F);
+                solidBrush.Dispose();
+            }
+
+            // Draw the bomb combo.
+            if (bombCombo > 0 && bombComboTimeCountdown >= 0)
+            {
+                solidBrush = new SolidBrush(Color.Firebrick);
+                e.Graphics.DrawString(String.Format("{0} Combo!", bombCombo),
+                    Font, solidBrush, 15.0F, 45.0F);
+                e.Graphics.DrawString(String.Format("{0}", bombComboScore),
+                    Font, solidBrush, 19.0F, 57.0F);
+                solidBrush.Dispose();
+            }
+
+            // Show the end-pattern result.
+            if (transitionFrames > 0 && !gameOver)
+            {
+                Font bigFont = new Font("Courier", 16, FontStyle.Bold);
+                solidBrush = new SolidBrush(Color.RoyalBlue);
+                e.Graphics.DrawString(beatThisPattern ? "Pattern Success!" :
+                    "Survival Failure...", bigFont, solidBrush, 60.0F, 50.0F);
+                e.Graphics.DrawString(beatThisPattern ? "30,000" : "5,000",
+                    bigFont, solidBrush, 100.0F, 70.0F);
+                solidBrush.Dispose();
+            }
+
+            // Show Game Over Result.
+            if (gameOver)
+            {
+                Font bigFont = new Font("Courier", 16, FontStyle.Bold);
+                solidBrush = new SolidBrush(Color.Crimson);
+                e.Graphics.DrawString("Game Over", bigFont, solidBrush,
+                    90.0F, 70.0F);
+                e.Graphics.DrawString("Please Press Shoot", Font, solidBrush,
+                    110.0F, 95.0F);
+                solidBrush.Dispose();
+            }
+             */
         }
     }
 }
